3e619e92abc98637a706de3f442d187e334a3d2b
ganlin.lu <ganlin.lu@zkjin.com>
2021-12-18 19:18:52 +0800

    add emebed struct test cases

--- test/types/aggregate/struct_test.go
+++ test/types/aggregate/struct_test.go
@@ -4,15 +4,16 @@ import (
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestStruct(t *testing.T) {
 	//1. 创建
 	//1.1 创建Struct Type
 	type Person struct {
-		name string
-		addr string
-		age int
+		name   string
+		addr   string
+		age    int
 		mobile string
 	}
 
@@ -30,16 +31,39 @@ func TestStruct(t *testing.T) {
 	//2.1 通过变量直接调用
 	assert.Equal(t, "MiXiaoquan", p2.name)
 
+	//2.2 通过指针调用
 	p2p := &p2
 	assert.Equal(t, "MiXiaoquan", p2p.name)
 	assert.Equal(t, "MiXiaoquan", (*p2p).name)
 
+	//3. one more thing
+	//3.1 嵌套struct
+	type Order struct {
+		orderTime time.Time
+		name      string
+		category  string
+		buyer     Person
+	}
 
+	order := Order{buyer: Person{name: "xiaomi", age: 8, addr: "minhang"}, name: "computer", category: "3c"}
+	assert.Equal(t, "3c", order.category)
+	assert.Equal(t, "computer", order.name)
+	assert.Equal(t, 8, order.buyer.age)
 
+	//3.2 匿名成员
+	type Department struct {
+		name, description string
+		Person
+	}
 
+	department := Department{
+		name:        "R&D",
+		description: "development",
+		Person: Person{name: "xiaomi", age: 8, addr: "minhang"},
+	}
 
-
-
-
+	assert.Equal(t, "R&D", department.name)
+	assert.Equal(t, "development", department.description)
+	assert.Equal(t, "minhang", department.addr)
 
 }
