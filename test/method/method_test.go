0dd475a1768f706f237b88f9ed514f5fc140c6d9
ganlin.lu <ganlin.lu@zkjin.com>
2021-12-29 16:12:42 +0800

    add more test cases for method test

--- test/method/method_test.go
+++ test/method/method_test.go
@@ -59,17 +59,33 @@ func TestMethod(t *testing.T) {
 	allen := Person{name: "Allen", mobile: "1810191282982", age: 17, Address: Address{city: "shanghai", district: "minhang", detail: "qibao"}}
 
 	//2 如何使用
-	assert.True(t, jack.isAdult()) // 2.1普通调用，调用者和被调用者都是struct
-	assert.True(t, (&jack).isAdult()) // 2.2 调用者是指针，被调用者是struct
 
-	assert.False(t, allen.isAdult2()) //2.3 调用者是struct,被调用者是pointer
-	assert.False(t, (&allen).isAdult2()) // 调用者和被调用者都是pointer
+	//2.1 常用调用方式
+	assert.True(t, jack.isAdult()) // a.普通调用，调用者和被调用者都是struct
+	assert.True(t, (&jack).isAdult()) // b.调用者是指针，被调用者是struct
 
+	assert.False(t, allen.isAdult2()) // c.调用者是struct,被调用者是pointer
+	assert.False(t, (&allen).isAdult2()) // d.调用者和被调用者都是pointer
+
+	//2.2 嵌套方法
 	//嵌套struct的方法调用
 	assert.Equal(t, float64(10000), jack.job.getSalary())
 
 	//嵌套struct的匿名防范调用
 	assert.Equal(t, "pudong", jack.getDistrict())
+
+	//2.3 method value & method expression
+
+	jackAdultFunc := jack.isAdult //a. method value, 实例方法
+	assert.True(t, jackAdultFunc())
+
+	adultFunc := Person.isAdult //b. method expression, 类方法
+	assert.True(t, adultFunc(jack))
+	assert.False(t,  adultFunc(allen))
+
+
+
+
 
 }
 
